2911d1967cd3d668198a123dbb0c9eb3eb731b68
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-04-22 12:51:18 +0200

    Tile Switcher function
    Javascript function for tile switcher

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,21 @@
 // write cool JS hwere!!
+const mainImage = document.getElementById('mainImage')
+const smallImageOne = document.getElementById('smallImageOne')
+const smallImageTwo = document.getElementById('smallImageTwo')
+const smallImageThree = document.getElementById('smallImageThree')
+const smallImageFour = document.getElementById('smallImageFour')
+console.log(smallImageOne);
+function imageSwitcher() {
+    let mainImageSrc = mainImage.src
+    let eventImageSrc = event.target.src
+
+    mainImage.src = eventImageSrc
+    event.target.src = mainImageSrc
+    console.log('asd')
+}
+
+
+smallImageOne.addEventListener('click', imageSwitcher)
+smallImageTwo.addEventListener('click', imageSwitcher)
+smallImageThree.addEventListener('click', imageSwitcher)
+smallImageFour.addEventListener('click', imageSwitcher)
